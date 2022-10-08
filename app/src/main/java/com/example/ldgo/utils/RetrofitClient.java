74abe64defcf04e4b1ceff317469fe90f5a51408
itumeleng <thiotsoela@gmail.com>
2022-10-08 10:04:53 +0200

    styping inputs

--- app/src/main/java/com/example/ldgo/utils/RetrofitClient.java
+++ app/src/main/java/com/example/ldgo/utils/RetrofitClient.java
@@ -1,16 +1,28 @@
 package com.example.ldgo.utils;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.Toast;
+
+import com.example.ldgo.HomeActivity;
+import com.example.ldgo.LoginActivity;
+import com.example.ldgo.entities.User;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit;
     private static final String BASE_URL = "https://ldgo-api.herokuapp.com/api/";
+    private static SharedPreferences sp;
 
     public static Retrofit getRetrofitInstance(){
 
